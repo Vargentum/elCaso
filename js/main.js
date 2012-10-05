d62ffc394ea84bb65d8c2febee0ead6e9d5d8116
Vargentum <vargentum@gmail.com>
2012-10-05 19:15:54 +0300

    Final Version 2.0
    Почищенны неиспользуемые файлы
    В папку sass добавлен readme
    Изменены настройки слайдера для более корректной работы.
    Добавлены правила для элементов со шрифтом Требушет.

--- js/main.js
+++ js/main.js
@@ -6,10 +6,10 @@ $(function(){
         animationSpeed: 500,
         autoplay: true,
         autoplaySpeed: 3000,
-        pauseOnHover: false,
+        pauseOnHover: true,
         bullets: true,
         arrows: false,
-        arrowsHide: true,
+        arrowsHide: false,
         prev: 'prev',
         next: 'next',
         animationStart: function(){},
@@ -26,10 +26,6 @@ $(function(){
         })
     }
 
-});
-
-
-$(function(){
     var thumbs = $('.product__thumbImage__item');
     var detailed = $('.product__detailImage .item');
     $(thumbs).each(function(){
@@ -43,37 +39,3 @@ $(function(){
         });
     });
 });
-
-
-//            $(thumbs).removeClass('m-active');
-//            $(detailed).removeClass('m-active');
-//            for(var i=1;i<=thumbs.length;i++){
-//                var t = 'item-' +[i];
-//                var d = '.product__detailImage__item-' +[i];
-//                if($(this).hasClass(t)){
-//                    $(this).addClass('m-active');
-//                    $(d).addClass('m-active');
-//                }
-//                else{return false;}
-//            }
-//            if($(this).hasClass('item-1')){
-//                $(this).addClass('m-active');
-//                $('.product__detailImage__item-1').addClass('m-active');
-//            }
-//            else if($(this).hasClass('item-2')){
-//                $(this).addClass('m-active');
-//                $('.product__detailImage__item-2').addClass('m-active');
-//            }
-//            else if($(this).hasClass('item-3')){
-//                $(this).addClass('m-active');
-//                $('.product__detailImage__item-3').addClass('m-active');
-//            }
-//            else if($(this).hasClass('item-4')){
-//                $(this).addClass('m-active');
-//                $('.product__detailImage__item-4').addClass('m-active');
-//            }
-//            else if($(this).hasClass('item-5')){
-//                $(this).addClass('m-active');
-//                $('.product__detailImage__item-5').addClass('m-active');
-//            }
-//            else{return false}
